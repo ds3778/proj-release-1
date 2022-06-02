af00b27ed2fa61f0cceeedbb83bf6281fc3f6924
Divya Soman <divya_soman@comcast.com>
2022-06-02 11:04:27 -0400

    deliverable 5

--- Release4.md
+++ Release4.md
@@ -1,20 +1,6 @@
-# Quasar App (proj-Release-3)
+# Quasar App (proj-Release-4)
 
-This github repository includes two project:
+In this release, I have added Login Page which has two buttons -Authenticate and
+Not Authentic. When clicking on Authentic button, user will be autneticated to access /user api of Github and will see the plan object in response. When unauthenticated, user will get a 401 response.
 
-1. backend microservice in folder ![microservice](/microservice)
-
-   Microservice is dockerized. To run the service, run the below docker commands:
-
-   `docker build -t backend_service_dockerized .`
-
-   `docker run -p 55000:9094 -d backend_service_dockerized`
-
-2. frontend ui project
-
-   To run the frontend service, please run the command, `quasar dev`, for details, refer ![README.md](/README.md)
-
-Once the microservice is up and running, my repository details can be seen in 'My Repos' tab in UI. This tab displays GitHub https://github.com/ds3778/
-
-Architecture diagram for the application:
-![Architecture diagram](/src/assets/architecture-diagram.png)
+I have already integerated Gists with github through backend service and table shows Gists in my repo in MyGists page.
--- src/pages/AppPages/Login.vue
+++ src/pages/AppPages/Login.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="q-pa-md">
     <q-banner class="bg-primary text-white">
-      <h6>Authenticated vs Unauthenticated</h6>
+      <h5>Authenticated vs Unauthenticated</h5>
+      <p>
+        Plan object can be seen when calling github api /users if authenticated.
+        Click on <b>Authenticate</b> button below to see
+      </p>
+      <p>
+        Plan object will be absent for unauthenticated user when calling github
+        api /users. Click on <b>Not Authentic</b> button below to see
+      </p>
     </q-banner>
 
     <div class="q-pa-md q-gutter-sm">
-      <q-btn @click="Increment" color="primary" label="Authenticate"></q-btn>
-      <q-btn color="secondary" label="Unauthorized" />
-      <p>{{ count }}</p>
+      <q-btn @click="authorize" color="primary" label="Authenticate"></q-btn>
+      <q-btn @click="notauthorize" color="secondary" label="Not Authentic" />
+      <p>
+        <b>Authenticate response: </b>
+        {{
+          JSON.stringify(
+            userData != null
+              ? userData.data != null
+                ? userData.data.plan
+                : undefined
+              : undefined
+          )
+        }}
+      </p>
+      <p>
+        <b>Not Authentic response </b>
+        {{
+          JSON.stringify(
+            unauthorizedData != null ? unauthorizedData : undefined
+          )
+        }}
+      </p>
     </div>
   </div>
   <!-- About, Background, Course, Publications, Photo-->
@@ -15,16 +42,69 @@
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue';
-import { api } from 'boot/axios';
+const axios = require('axios').default;
 export default defineComponent({
   name: 'LoginPage',
   setup() {
-    const count = ref(0);
-    const Increment = async () => {
-      const res = await api.get('user');
-      console.log(res);
+    type userType = {
+      data: dataType;
     };
-    return { count, Increment };
+    type dataType = {
+      login: string;
+      plan: planType;
+    };
+    type planType = {
+      name: string;
+      space: string;
+      collaborators: string;
+      private_repos: string;
+    };
+
+    const unauthorizedData = ref({});
+    const notauthorize = async () => {
+      try {
+        const user: userType = await axios.get('https://api.github.com/user', {
+          headers: {
+            Accept: 'application/vnd.github.v3+json',
+          },
+        });
+
+        unauthorizedData.value = user;
+        return unauthorizedData;
+      } catch (error) {
+        console.log(
+          `Error in calling github api in notauthorize method ${error}`
+        );
+        userData.value = '';
+        unauthorizedData.value =
+          "Not Authorized, Please click on 'Authenticate'";
+      }
+    };
+    const userData = ref({});
+    const authorize = async () => {
+      try {
+        const user: userType = await axios.get('https://api.github.com/user', {
+          headers: {
+            authorization:
+              'Basic ' +
+              btoa('ds3778' + ':' + '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'),
+          },
+        });
+        console.log(JSON.stringify(user.data.plan));
+        userData.value = user;
+        unauthorizedData.value = '';
+        return userData;
+      } catch (error) {
+        console.log('Error in calling github api in authorize method');
+      }
+    };
+
+    return {
+      userData,
+      authorize,
+      notauthorize,
+      unauthorizedData,
+    };
   },
 });
 </script>
